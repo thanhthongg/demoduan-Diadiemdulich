f7487241c378121915f7a0eb7d04e7cc3af6b564
Blussom <Blussom@DESKTOP-02CJCK5>
2022-04-05 09:15:34 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demoDuAn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

9d25c9716b2f34b9d200f2db3443dac2273090fc
thanhthongg <blussomthong1912@gmail.com>
2022-04-18 22:43:33 +0700

    Added new

---
+++ Controllers/DiemDenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demoDuAn.Controllers
+{
+    public class DiemDenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using demoDuAn.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
